799358eb4925857a7d51158c574067993489a8ab
Ian <ianjchurch1@gmail.com>
2025-10-31 08:43:16 -0700

    Initial Commit

---
+++ Home Wall/Home_WallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_WallApp.swift
+//  Home Wall
+//
+//  Created by Ian Church on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Home_WallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Home WallUITests/Home_WallUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Home_WallUITestsLaunchTests.swift
+//  Home WallUITests
+//
+//  Created by Ian Church on 10/31/25.
+//
+
+import XCTest
+
+final class Home_WallUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
